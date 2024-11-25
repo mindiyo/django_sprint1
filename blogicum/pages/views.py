0ac95f0d80e8c1381f4c4708b1c54186d07d2a42
Диана <dianaviz@mail.ru>
2024-11-25 13:42:50 +0300

    Исправлены ошибки PEP 8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 def about(request):
     return render(request, 'about.html')  # Отображаем шаблон about.html
+
 
 def rules(request):
     return render(request, 'rules.html')  # Отображаем шаблон rules.html
